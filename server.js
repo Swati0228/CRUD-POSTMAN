c5e314325d18ca56bb7004d3062158fdeec495c2
Swati Singh <swatisingh022805@gmail.com>
2026-05-01 18:46:07 +0530

    Setting up the server backend

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
-.env
-node_modules/
+const express = require('express');
+const app = express();
+const port = 8080;
+
+// Define a route for GET requests to the root URL
+app.get('/', (req, res) => {
+  res.send('Hello World from Express!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
