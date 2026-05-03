e14ca0a2ac4d607c7c237eae35d30ae448a4728c
Swati Singh <swatisingh022805@gmail.com>
2026-05-03 22:52:32 +0530

    connection to mongoose

--- server.js
+++ server.js
@@ -2,12 +2,17 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-// Define a route for GET requests to the root URL
+const connectDB = require('./config/db');
+
+// ✅ Connect database
+connectDB();
+
+// Define a route
 app.get('/', (req, res) => {
   res.send('Hello World from Express!');
 });
 
-// Start the server
+// Start server
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
